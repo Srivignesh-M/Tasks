7e0f2047b90f316528872eaebcd96e6bf5b6b178
Srivignesh-M <srivigneshm.ug22.cs@francisxavier.ac.in>
2026-02-02 16:40:52 +0530

    Executed using the prepared statement

--- JDBC.java
+++ JDBC.java
@@ -1,19 +1,30 @@
 package Tasks;
 import java.sql.*;
+import java.util.Scanner;
 public class JDBC {
 	public static void main(String[] args) {
 	String Url="jdbc:mysql://localhost:3306/vikky";
 	String user="root";
-	String pass="*********";
+	String pass="";
+	Scanner s=new Scanner(System.in);
+	int id=s.nextInt();
+	String name=s.next();
+	s.nextLine();
+	String password=s.nextLine();
 	try{
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		Connection con=DriverManager.getConnection(Url,user,pass);
-		Statement s=con.createStatement();
-		String query="Insert into usersample values(2,\"vikky\",\"password\");";
-		s.executeUpdate(query);
-		ResultSet rs=s.executeQuery("select*from usersample");
+		String query="Insert into usersample values (?,?,?)";
+		PreparedStatement ps=con.prepareStatement(query);
+		ps.setInt(1,id );
+		ps.setString(2, name);
+		ps.setString(3, password);
+		int updated=ps.executeUpdate();
+		String sql="select * from usersample";
+		PreparedStatement ps2=con.prepareStatement(sql);
+		ResultSet rs=ps2.executeQuery();
 		while(rs.next()) {
-			System.out.println(rs.getString("id")+" "+rs.getString("name")+" "+rs.getString("password"));
+			System.out.println(rs.getInt("id")+" "+rs.getString("name")+" "+rs.getString("password"));
 	}
 	}
 	catch(Exception e) {
