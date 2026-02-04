7dd681c2b37cb26820a9c74170179bd200d37b66
Srivignesh-M <srivigneshm.ug22.cs@francisxavier.ac.in>
2026-02-04 13:16:13 +0530

    changes in thread

--- MyThread.java
+++ MyThread.java
@@ -4,10 +4,10 @@ public class MyThread {
 
 	public static void main(String[] args) throws Exception{
 		Custom_Thread1 t1=new Custom_Thread1();
-		Custom_Thread2 t2=new Custom_Thread2();
+		Custom_Thread1 t2=new Custom_Thread1();
 		t1.start();
 		t2.start();
-		t1.sleep(30);
+		t1.sleep(300);
 //		t2.sleep(300);
 	}
 
